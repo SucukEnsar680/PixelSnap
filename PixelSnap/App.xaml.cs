24ee9be20d7607bf6f64d33f822098b7557e1405
Ensar Bugday <bilal-ensar.bugday@student.htl-rankweil.at>
2025-02-03 08:11:51 +0100

    Add project files.

---
+++ PixelSnap/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PixelSnap
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PixelSnap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PixelSnap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
