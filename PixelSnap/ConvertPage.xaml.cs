4f83a6f0d028053d5693e0ffaa10f3af0c26fa48
Thomas <thomas.schallert@gmx.at>
2025-02-04 09:52:31 +0100

    Finished convert page but imageconverter needs to much ram

--- PixelSnap/ConvertPage.xaml.cs
+++ PixelSnap/ConvertPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.Layouts;
+using SkiaSharp;
 
 namespace PixelSnap;
 
@@ -48,6 +49,7 @@ public partial class ConvertPage : ContentPage
                 if (file != null)
                 {
                     image.Source = ImageSource.FromFile(file.FullPath);
+                    ImagePath = file.FullPath;
                 }
             })
         });
@@ -64,19 +66,33 @@ public partial class ConvertPage : ContentPage
 
         button.Clicked += (sender, e) =>
         {
-            Imagec;
+            SKBitmap bitmap =  PixelArtConverter.ReduceColors(ImagePath,8);
+            string convImg = SaveImage(bitmap);
+            Image image_con = new Image
+            {
+                Source = ImageSource.FromFile(convImg),
+                WidthRequest = double.NaN,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Aspect = Aspect.AspectFit,
+            };
+            flexLayout.Children.Add(image_con);
+
+
         };
 
         flexLayout.Children.Add(button);
-        Image image_con = new Image
-        {
-            Source = ImageSource.FromFile(ImagePath),
-            WidthRequest = double.NaN,
-            HeightRequest = 200,
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center,
-            Aspect = Aspect.AspectFit,
-        };
-        flexLayout.Children.Add(image_con);
+    }
+    private static string SaveImage(SKBitmap bitmap)
+    {
+
+        string galleryPath = Path.Combine(FileSystem.AppDataDirectory, "gallery", "images");
+        Directory.CreateDirectory(galleryPath); // Ensure directory exists
+
+        string fileName = $"image_{DateTime.Now:yyyyMMdd_HHmmss}.jpg";
+        string imagePath = Path.Combine(galleryPath, fileName);
+        PixelArtConverter.SaveImage(bitmap, imagePath);
+        return imagePath;
     }
 }
