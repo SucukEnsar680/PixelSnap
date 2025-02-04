ddaf0774dad9047a411c70534205e2bb8d18176f
dinomarlon187 <marlon.pichler@student.htl-rankweil.at>
2025-02-04 09:01:11 +0100

    Added Toolkit

--- PixelSnap/MauiProgram.cs
+++ PixelSnap/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace PixelSnap
 {
@@ -9,6 +10,7 @@ namespace PixelSnap
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
