2e6e46872bc2c2d5cfa2d41fb9826b0ad5a680d3
dinomarlon187 <marlon.pichler@student.htl-rankweil.at>
2025-02-05 11:18:09 +0100

    Hotfix

--- PixelSnap/MainPage.xaml.cs
+++ PixelSnap/MainPage.xaml.cs
@@ -11,13 +11,17 @@ namespace PixelSnap
             InitializeComponent();
             this.cameraProvider = cameraProvider;
         }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
         public async void Open_Gallery(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Page_Gallery());
         }
         public async void Open_Converter(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new ConvertPage());
+            await Navigation.PushAsync(new CameraViewPage(cameraProvider));
         }
     }
 
