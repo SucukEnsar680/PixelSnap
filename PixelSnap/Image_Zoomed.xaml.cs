9c25de9f06c464d6595774c7fdea8d2329b1168f
Thomas <thomas.schallert@gmx.at>
2025-02-05 10:24:07 +0100

    added the share function for images

--- PixelSnap/Image_Zoomed.xaml.cs
+++ PixelSnap/Image_Zoomed.xaml.cs
@@ -2,23 +2,48 @@ namespace PixelSnap;
 
 public partial class Image_Zoomed : ContentPage
 {
-	string Imagepath { get; set; }
-	public Image_Zoomed(string imagepath)
-	{
-		InitializeComponent();
+    string Imagepath { get; set; }
+
+    public Image_Zoomed(string imagepath)
+    {
+        InitializeComponent();
         Imagepath = imagepath;
         DrawCurrentImage(Imagepath);
     }
-	public void DrawCurrentImage(string imagepath)
-	{
-		ZoomedImage.Source = ImageSource.FromFile(imagepath);
+
+    public void DrawCurrentImage(string imagepath)
+    {
+        ZoomedImage.Source = ImageSource.FromFile(imagepath);
     }
-	public void OnBackButtonClicked(object sender, EventArgs e)
+
+    public void OnBackButtonClicked(object sender, EventArgs e)
     {
         Navigation.PopAsync();
     }
-	public void OnMenuButtonClicked(object sender, EventArgs e)
+
+    public void OnMenuButtonClicked(object sender, EventArgs e)
     {
         Navigation.PopToRootAsync();
+    }
+
+    public async void OnShareButtonClicked(object sender, EventArgs e)
+    {
+        await ShareImageAsync(Imagepath);
+    }
+
+    private async Task ShareImageAsync(string imagePath)
+    {
+        try
+        {
+            await Share.Default.RequestAsync(new ShareFileRequest
+            {
+                Title = "Share Image",
+                File = new ShareFile(imagePath)
+            });
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", $"Failed to share: {ex.Message}", "OK");
+        }
     }
 }
